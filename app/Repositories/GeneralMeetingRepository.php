9cab0156b3a858ae76e2c2a04906d58c107ddbb6
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-04-07 16:46:46 +0100

    maj ag

--- app/Repositories/GeneralMeetingRepository.php
+++ app/Repositories/GeneralMeetingRepository.php
@@ -50,20 +50,20 @@ class GeneralMeetingRepository
     public function attachement($request) {
 
         $general_meeting = GeneralMeeting::find($request->general_meeting_id);
-        $fieldName = GeneralMeeting::TYPE_FILE_FIELD_VALUE[$request->files['type']];
+        $fieldName = GeneralMeeting::TYPE_FILE_FIELD_VALUE[$request['files']['type']];
 
         $generalMeeting = new GeneralMeeting();
         if ($generalMeeting->isFillable($fieldName)) {
 
             $general_meeting->update([
-                $fieldName => uploadFile( $request->files['file'], 'ag_documents'),
+                $fieldName => uploadFile( $request['files']['file'], 'ag_documents'),
                 GeneralMeeting::DATE_FILE_FIELD[$fieldName] => now(),
             ]);
         }else {
 
             $fileUpload = new GourvernanceDocument();
 
-            $fileUpload->file = uploadFile($request->files['file'], 'ag_documents');
+            $fileUpload->file = uploadFile($request['files']['file'], 'ag_documents');
             $fileUpload->status = $general_meeting->status;
 
             $general_meeting->fileUploads()->save($fileUpload);
