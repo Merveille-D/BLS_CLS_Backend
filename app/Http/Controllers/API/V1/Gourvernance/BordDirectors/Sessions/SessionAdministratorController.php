a1886e1c90a86992d2b99a55cbc95670b2bba753
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-04-08 17:59:56 +0100

    maj ca

--- app/Http/Controllers/API/V1/Gourvernance/BordDirectors/Sessions/SessionAdministratorController.php
+++ app/Http/Controllers/API/V1/Gourvernance/BordDirectors/Sessions/SessionAdministratorController.php
@@ -21,11 +21,15 @@ class SessionAdministratorController extends Controller
      */
     public function index()
     {
-        $session_administrators = SessionAdministrator::when(request('status') != null, function($query) {
+        $session_administrator = SessionAdministrator::when(request('status') != null, function($query) {
             $query->where('status', request('status'));
-        })->get();
+        })->firstt();
 
-        return api_response(true, "Liste des Sessions", $session_administrators, 200);
+        $data = $session_administrator->toArray();
+        $data['files'] = $session_administrator->files;
+        $data['next_task'] = $session_administrator->next_task;
+
+        return api_response(true, "Liste des Sessions", $data, 200);
     }
 
     /**
