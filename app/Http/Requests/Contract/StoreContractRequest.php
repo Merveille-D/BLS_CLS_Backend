e81c9b03462be2778caf301c9d84802e0507fb93
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-04-13 18:00:51 +0100

    fix bugs contracts

--- app/Http/Requests/Contract/StoreContractRequest.php
+++ app/Http/Requests/Contract/StoreContractRequest.php
@@ -32,11 +32,46 @@ class StoreContractRequest extends FormRequest
             'type_category' => [ Rule::in(Contract::TYPE_CATEGORIES[$request->input('category')]) ],
             'contract_file' => ['required', 'file'],
 
+            // 'first_part' => ['required', 'array'],
+            // 'first_part.*.part_id' => ['required', 'uuid', 'exists:parts,id'],
+            // 'first_part.*.description' => ['required', 'string'],
+            // 'second_part' => ['required', 'array'],
+            // 'second_part.*.part_id' => ['required', 'uuid', 'exists:parts,id'],
+            // 'second_part.*.description' => ['required', 'string'],
+
             'first_part' => ['required', 'array'],
-            'first_part.*.part_id' => ['required', 'uuid', 'exists:parts,id'],
+            'first_part.*.part_id' => [
+                'required',
+                'uuid',
+                'exists:parts,id',
+                function ($attribute, $value, $fail) {
+                    $secondPartData = request()->input('second_part');
+
+                    foreach ($secondPartData as $item) {
+                        if ($item['part_id'] === $value) {
+                            $fail('Le part_id ne peut pas être présent à la fois dans first_part et second_part');
+                        }
+                    }
+                },
+            ],
             'first_part.*.description' => ['required', 'string'],
+
+            
             'second_part' => ['required', 'array'],
-            'second_part.*.part_id' => ['required', 'uuid', 'exists:parts,id'],
+            'second_part.*.part_id' => [
+                'required',
+                'uuid',
+                'exists:parts,id',
+                function ($attribute, $value, $fail) {
+                    $firstPartData = request()->input('first_part');
+
+                    foreach ($firstPartData as $item) {
+                        if ($item['part_id'] === $value) {
+                            $fail('Le part_id ne peut pas être présent à la fois dans first_part et second_part');
+                        }
+                    }
+                },
+            ],
             'second_part.*.description' => ['required', 'string'],
 
         ];
