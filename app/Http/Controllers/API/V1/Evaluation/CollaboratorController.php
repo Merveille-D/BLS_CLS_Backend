76331180902ca57ccf091a4336048f7b9d2469c9
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-04-21 02:46:41 +0100

    fix bugs

--- app/Http/Controllers/API/V1/Evaluation/CollaboratorController.php
+++ app/Http/Controllers/API/V1/Evaluation/CollaboratorController.php
@@ -24,6 +24,7 @@ class CollaboratorController extends Controller
     public function index(ListCollaboratorRequest $request)
     {
         $collaborators = Collaborator::all();
+        
         return api_response(true, "Liste des indicateurs de performance", $collaborators, 200);
     }
 
