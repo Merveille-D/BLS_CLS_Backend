af89bf70812d14b96a686f31be0a14b0bc572cd5
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-04-28 13:04:38 +0100

    fix bugs alert

--- app/Http/Resources/Alert/NotificationResource.php
+++ app/Http/Resources/Alert/NotificationResource.php
@@ -11,7 +11,7 @@ class NotificationResource extends JsonResource
         return [
             'id' => $this->id,
             // 'state' => $this->state,
-            'priority' => $this->priority,
+            'priority' => $this->alert->priority,
             'sent_by' => $this->sent_by,
             'sent_to' => $this->sent_to,
             'title' => $this->data['title'],
