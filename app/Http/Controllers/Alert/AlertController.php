070b3c17781f273da9bd9123f990fcc8a79cffaa
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-04-29 01:15:37 +0100

    fix bugs alert

--- app/Http/Controllers/Alert/AlertController.php
+++ app/Http/Controllers/Alert/AlertController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Alert;
 
 use App\Http\Controllers\Controller;
-use App\Models\Alert\Alert;
+use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
 use App\Repositories\Alert\AlertRepository;
 use Illuminate\Validation\ValidationException;
 
@@ -19,6 +20,12 @@ class AlertController extends Controller
         return api_response( $response, "Resultat de l'envoi des alertes", null, 200);
     }
 
+    public function startQueueWorker(Request $request)
+    {
+        $process = new Process(['php', 'artisan', 'queue:work']);
+        $process->start();
 
+        return api_response( true, "Lancement de la file d'attente ", null, 200);
+    }
 
 }
--- routes/V1/alert.php
+++ routes/V1/alert.php
@@ -9,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 Route::resource('notifications', NotificationController::class);
 // Route::resource('alerts', AlertController::class);
 Route::get('trigger_module_alert', [AlertController::class, 'triggerModuleAlert'] );
+Route::get('start-queue-worker', [AlertController::class, 'startQueueWorker'] );
