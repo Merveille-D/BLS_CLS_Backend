ebe65e98883968ab2bddeae113e4d547dfa3b877
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-05-01 14:55:12 +0100

    fix bugs shareholders

--- app/Concerns/Traits/Transfer/AddTransferTrait.php
+++ app/Concerns/Traits/Transfer/AddTransferTrait.php
@@ -3,13 +3,10 @@
 namespace App\Concerns\Traits\Transfer;
 
 use App\Models\Transfer\Transfer;
-<<<<<<< HEAD
 use Carbon\Carbon;
-=======
 use DateInterval;
 use DateTime;
 use DateTimeImmutable;
->>>>>>> a6e3f6acdcd086cebfb7e70d8a1e4b5168e9b49a
 use Illuminate\Database\Eloquent\Model;
 
 trait AddTransferTrait
@@ -25,14 +22,8 @@ trait AddTransferTrait
      * @param  mixed $collaborators
      * @return void
      */
-<<<<<<< HEAD
-    public function add_transfer(Model $model, string $title, $deadline, $description = null, $collaborators) : void {
-=======
     public function add_transfer(Model $model, string $title, $deadline, $description = null, $collaborators): void
     {
-        $dates = $this->getMilestoneDates($deadline);
->>>>>>> a6e3f6acdcd086cebfb7e70d8a1e4b5168e9b49a
-
         $transfer = new Transfer();
         $transfer->title = $title;
         $transfer->description = $description ?? null;
