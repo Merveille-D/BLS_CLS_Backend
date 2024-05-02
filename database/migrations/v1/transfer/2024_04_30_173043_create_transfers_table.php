ddbcbee211e533f24550499f610373b36489f0e9
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-05-02 09:46:23 +0100

    fix bugs alert

--- database/migrations/v1/transfer/2024_04_30_173043_create_transfers_table.php
+++ database/migrations/v1/transfer/2024_04_30_173043_create_transfers_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->foreign('sender_id')->references('id')->on('users');
             $table->timestamps();
         });
+        
     }
 
     /**
