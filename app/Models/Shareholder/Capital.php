d33ebbcb32bfc5a4948c645a98a745ee4c992096
AGOSSADOU Elognisse Erasme Guérin <55337134+agossadourin@users.noreply.github.com>
2024-05-03 08:49:35 +0100

    fix bugs alert

--- app/Models/Shareholder/Capital.php
+++ app/Models/Shareholder/Capital.php
@@ -2,10 +2,12 @@
 
 namespace App\Models\Shareholder;
 
+use App\Observers\CapitalObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+#[ObservedBy([CapitalObserver::class])]
 class Capital extends Model
 {
     use HasFactory, HasUuids;
