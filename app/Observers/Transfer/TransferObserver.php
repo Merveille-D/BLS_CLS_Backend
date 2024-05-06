50b7a8be725ff9aff8995d153af7ad8e2a1ea7d4
Raoul G <raoulgbadou@gmail.com>
2024-05-06 10:13:06 +0100

    fix : uncomment transfer alert generation

--- app/Observers/Transfer/TransferObserver.php
+++ app/Observers/Transfer/TransferObserver.php
@@ -15,10 +15,11 @@ class TransferObserver implements ShouldHandleEventsAfterCommit
      */
     public function created(Transfer $transfer): void
     {
-        // $dates = $this->getMilestoneDates($transfer->deadline);
-        // foreach ($dates as $key => $date) {
-        //     $this->new_alert($transfer, $transfer->title, $transfer->description, 'transfer', $date, Alert::STATUS[$key]);
-        // }
+        $dates = $this->getMilestoneDates($transfer->deadline);
+
+        foreach ($dates as $key => $date) {
+            $this->new_alert($transfer, $transfer->title, $transfer->description, 'transfer', $date, Alert::STATUS[$key]);
+        }
 
     }
 
