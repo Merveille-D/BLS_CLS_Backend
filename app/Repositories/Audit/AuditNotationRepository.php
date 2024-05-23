25f4572feeafdb761a76a15be27fbc03778c7f4d
Julian-root <adjibiadechinan@gmail.com>
2024-05-23 18:01:14 +0100

    fix bugs category model contract

--- app/Repositories/Audit/AuditNotationRepository.php
+++ app/Repositories/Audit/AuditNotationRepository.php
@@ -15,7 +15,8 @@ class AuditNotationRepository
 
     public function all() {
         return $this->audit_notation->whereNull('parent_id')->get()->makeHidden('performances')->map(function ($audit_notation) {
-            $audit_notation->title = $audit_notation->title;
+
+            // $audit_notation->title = $audit_notation->title;
 
             unset(
                 $audit_notation->status,
