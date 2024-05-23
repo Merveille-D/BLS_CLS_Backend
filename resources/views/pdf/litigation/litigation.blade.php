968a870534fc85a7354f3362ac759a3d381c3a65
Raoul G <raoulgbadou@gmail.com>
2024-05-23 15:24:48 +0100

    fix generating litigation pdf

--- resources/views/pdf/litigation/litigation.blade.php
+++ resources/views/pdf/litigation/litigation.blade.php
@@ -1,137 +1,78 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Récapitulatif de Paiement</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 20px;
-            font-size: 12px;
-        }
-        .container {
-            width: 100%;
-            max-width: 800px;
-            margin: 0 auto;
-        }
-        .header {
-            text-align: center;
-            margin-bottom: 30px;
-        }
-        .header img {
-            width: 60px;
-        }
-        .header h1 {
-            font-size: 24px;
-            margin: 0;
-        }
-        .header .subtitle {
-            margin: 5px 0;
-        }
-        .info-table, .data-table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 20px;
-        }
-        .info-table th, .info-table td, .data-table th, .data-table td {
-            padding: 8px;
-            text-align: left;
-            border: 1px solid #ccc;
-        }
-        .data-table th {
-            background-color: #f2f2f2;
-        }
-        .footer {
-            text-align: right;
-            font-weight: bold;
-            font-size: 14px;
-        }
-        .highlight {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <div class="header">
-            <div>
-                <img src="{{ public_path('images/bls-logo.png') }}" alt="Logo">
-            </div>
-            <div>
-                <h1>Récapitulatif de Paiement</h1>
-                <p class="subtitle">Portail National des services publics</p>
-                <p class="subtitle">PS00140-220929-l8nf6wcc</p>
-                <p class="subtitle">jeudi 29 septembre 2022</p>
-            </div>
+@extends('pdf.layout')
+@section('content')
+<div class="container">
+    <div class="header">
+        <div class="left">
+            <h1>Fiche de suivi</h1>
+            <p class="subtitle bold">{{ $litigation->name }}</p>
+            <p class="subtitle italic underline">{{ $litigation->reference }}</p>
+            <p class="subtitle">{{ formatDateTime($litigation->created_at) }}</p>
         </div>
-        <table class="info-table">
-            <tr>
-                <td>
-                    <strong>SOUDÉ THÉODORE</strong><br>
-                    raoulgbadou@gmail.com<br>
-                    Référence abonné: 330501530258FA<br>
-                    Propriétaire du compteur: SOUDÉ THÉODORE
-                </td>
-                <td>
-                    <strong>Référence Paiement</strong><br>
-                    59303011
-                </td>
-                <td>
-                    <strong>Agrégrateur</strong><br>
-                    fedapay
-                </td>
-            </tr>
-        </table>
-        <table class="data-table">
-            <thead>
-                <tr>
-                    <th>Désignation</th>
-                    <th>Valeur</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Période</td>
-                    <td>Juillet 2022</td>
-                </tr>
-                <tr>
-                    <td>Numéro de facture</td>
-                    <td>FE631171209589</td>
-                </tr>
-                <tr>
-                    <td>Montant payé (SBEE)</td>
-                    <td>803 FCFA</td>
-                </tr>
-                <tr>
-                    <td>Numéro de Règlement</td>
-                    <td>126685</td>
-                </tr>
-                <tr>
-                    <td>Moyen de paiement</td>
-                    <td>fedapay</td>
-                </tr>
-                <tr>
-                    <td>Frais de transaction (fedapay)</td>
-                    <td>0 FCFA</td>
-                </tr>
-                <tr>
-                    <td>Source de paiement</td>
-                    <td>MTN</td>
-                </tr>
-                <tr>
-                    <td>Numéro transaction (MTN)</td>
-                    <td>3487552449</td>
-                </tr>
-            </tbody>
-        </table>
-        <div class="footer">
-            Montant Net payé <span class="highlight">803 FCFA</span>
-        </div>
-        <div class="footer">
-            Arrêté le présent reçu à la somme de: 803 FCFA
+        <div class="right">
+            <img src="{{ $base64Image }}" alt="Logo">
         </div>
     </div>
-</body>
-</html>
+
+    <h2>Détail du dossier</h2>
+    <table class="data-table">
+        {{-- <thead>
+            <tr>
+                <th>Désignation</th>
+                <th>Valeur</th>
+            </tr>
+        </thead> --}}
+        <tbody>
+            @foreach ($details as $key => $item)
+                <tr>
+                    <td>{{ $key }}</td>
+                    <td>{{ $item }}</td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+
+    <h2>Parties</h2>
+    <table class="data-table">
+        <thead>
+            <tr>
+                <th>Partie</th>
+                <th>Catégorie</th>
+                <th>Type</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($litigation->parties as $key => $party)
+                <tr>
+                    <td>{{ $party->name }}</td>
+                    <td>{{ $party->pivot->category  }}</td>
+                    <td>{{ $party->pivot->type  }}</td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+
+    <h2>Planification</h2>
+    <table class="data-table">
+        <thead>
+            <tr>
+                <th>Libellé</th>
+                <th>Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($litigation->tasks as $key => $task)
+                <tr  class="{{ $task->status ?: 'gray italic'}}">
+                    <td>{{ $task->title }}</td>
+                    <td>{{ $task->completed_at }}</td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+    <div class="footer" id="footer">
+        <span class="pagenum"></span>
+    </div>
+</div>
+@endsection
