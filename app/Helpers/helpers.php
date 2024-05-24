5927be5a12a9ca154f6e2a32616e84fb0b96dff3
Raoul G <raoulgbadou@gmail.com>
2024-05-24 21:22:26 +0100

    fix : remove random string from reference generating

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -41,7 +41,7 @@ if (!function_exists('generateReference')) {
     {
         $currentDate = date('YmdHis');
         $number = $model->withoutGlobalScopes()->count() + 1;
-        $reference = $prefix . '-' . completeWithZeros($number) . '-' . $currentDate.Str::random(6);
+        $reference = $prefix . '-' . completeWithZeros($number) . '-' . $currentDate;
 
         return $reference;
     }
