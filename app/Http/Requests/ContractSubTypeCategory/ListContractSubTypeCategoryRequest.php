c98c272dc8b6208af4344d56e45c4f3874724e4a
webcreation-dev <adjilan2403@gmail.com>
2024-10-23 18:02:09 +0100

    fix : name sub type category request

--- app/Http/Requests/ContractSubTypeCategory/ListContractSubTypeCategoryRequest.php
+++ app/Http/Requests/ContractSubTypeCategory/ListContractSubTypeCategoryRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\ContractTypeCategory;
+namespace App\Http\Requests\ContractSubTypeCategory;
 
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
